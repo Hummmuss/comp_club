9bb7909c56d3a2370ae5399caabcc6436d94f062
TsInella <firefly.goo5@gmail.com>
2023-11-22 01:28:05 +0300

    mainPage 2.0& compPricing

--- src/components/computers pricing/ComputersPricing.jsx
+++ src/components/computers pricing/ComputersPricing.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import style from './ComputersPricing.module.css'
+import styles from './ComputersPricing.module.css'
+import {Content} from "antd/es/layout/layout";
+import {Layout} from "antd";
+
 const ComputersPricing = () => {
     return (
-        <div>
+        <Layout className={styles.pricing_list}>
 
-        </div>
+            <Content>
+
+                <div className={styles.title}>Тарифы на компьютеры</div>
+                <div className={styles.title2}>Компьютеры в общем зале</div>
+                <div className={styles.title3}>RTX 2080 + i5 10400F
+                </div>
+                <div className={styles.titles45}>
+                <div className={styles.title4}>Будни
+                    <p>1 час - 100 р.<br/>
+                        3 часа - 250 р.<br/>
+                        5 часов - 450 р.<br/>
+                        Пакет “Утро” - 250 р.<br/>
+                        Пакет “Ночь” - 480 р.<br/>
+                    </p>
+                </div>
+                <div className={styles.title4_1}>Выходные
+                    <p>1 час - 110 р.<br/>
+                        3 часа - 300 р.<br/>
+                        5 часов - 450 р.<br/>
+                        Пакет “Утро” - 300 р. <br/>
+                        Пакет “Ночь” - 480</p>
+                </div>
+                </div>
+            </Content>
+        </Layout>
+
     );
 };
 
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './Main.module.css'
 import {Button, Layout} from "antd";
 import {Content} from "antd/es/layout/layout";
+
 const Main = () => {
     return (
-        <Layout className={styles.main}>
-            <Content>
+        <Layout  className={styles.main}>
+            <Content className={styles.form}>
                 <div className={styles.title}>CYBERCLUB <br />INPLAY</div>
+                {/*<img src='../../assets/Vector.png'/>*/}
                 <div className={styles.description}>Хочешь хорошо отдохнуть, прокачать <br /> свой скилл и поиграть с друзьями?<br />
                     Ты попал по адресу!</div>
                 <Button className={styles.button1} style={{color: 'white', fontFamily: 'Montserrat',
