ab63431e388f681feec1ab019ee82884b82d5c5b
TsInella <firefly.goo5@gmail.com>
2023-11-23 22:29:53 +0300

    ашалеть

--- src/components/header/Head.jsx
+++ src/components/header/Head.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import './Head.module.css';
-import { Layout, Menu } from 'antd';
-const { Header } = Layout;
+import {Layout, Menu} from 'antd';
+import styles from './Head.module.css'
+import logo from '../../assets/img.png'
+
+const {Header} = Layout;
 const Head = () => {
     return (
-        <Header style={{ width: '1525px', height: '77px', background: 'black' }}>
-            <Menu theme="dark" mode="horizontal" style = {{backgroundColor: 'black', justifyContent:'center'}}>
-                <Menu.Item key="about" style = {{backgroundColor: 'black'}}>О нас</Menu.Item>
-                <Menu.Item key="tariffs" style = {{backgroundColor: 'black'}}>Тарифы</Menu.Item>
-                <Menu.Item key="booking" style = {{backgroundColor: 'black'}}>Бронь</Menu.Item>
-                <Menu.Item key="gallery" style = {{backgroundColor: 'black'}}>Галерея</Menu.Item>
-                <Menu.Item key="contacts" style = {{backgroundColor: 'black'}}>Контакты</Menu.Item>
+        <Header style={{width: '1525px', height: '77px', background: 'black'}}>
+
+            {/*<img src={logo} style={{width: '10', height: '53'}}/>*/}
+            <Menu theme="dark" mode="horizontal" style={{backgroundColor: 'black', justifyContent: 'center'}}>
+                {/*<Menu.Item key = "logo" style={{ src: {logo}, width: '100px', height: 'auto' }}></Menu.Item>*/}
+                <Menu.Item key="about" style={{
+                    backgroundColor: 'black',
+                    fontSize: 24,
+                    color: 'white',
+                    fontFamily: 'Montserrat, sans-serif',
+                    fontStyle: 'normal',
+                    fontWeight: 500
+                }}>О нас</Menu.Item>
+                <Menu.Item key="tariffs" style={{
+                    backgroundColor: 'black',
+                    fontSize: 24,
+                    color: 'white',
+                    fontFamily: 'Montserrat sans-serif',
+                    fontStyle: 'normal',
+                    fontWeight: 500
+                }}>Тарифы</Menu.Item>
+                <Menu.Item key="booking" style={{
+                    backgroundColor: 'black',
+                    fontSize: 24,
+                    color: 'white',
+                    fontFamily: 'Montserrat sans-serif',
+                    fontStyle: 'normal',
+                    fontWeight: 500
+                }}>Бронь</Menu.Item>
+                <Menu.Item key="gallery" style={{
+                    backgroundColor: 'black',
+                    fontSize: 24,
+                    color: 'white',
+                    fontFamily: 'Montserrat sans-serif',
+                    fontStyle: 'normal',
+                    fontWeight: 500
+                }}>Галерея</Menu.Item>
+                <Menu.Item key="contacts" style={{
+                    backgroundColor: 'black',
+                    fontSize: 24,
+                    color: 'white',
+                    fontFamily: 'Montserrat sans-serif',
+                    fontStyle: 'normal',
+                    fontWeight: 500
+                }}>Контакты</Menu.Item>
             </Menu>
         </Header>
     );
